bee137058ae0e752232c9d1b6668b815591f0c85
Aline Teixeira <alinet.reis1@gmail.com>
2021-02-15 23:07:16 -0300

    Feito layout HTML/CSS

---
+++ script.js
@@ -0,0 +1,8 @@
+let order =[];
+let clickedOrder = [];
+let score = 0;
+
+const blue = document.querySelector['.blue'];
+const yellow = document.querySelector['.yellow'];
+const red = document.querySelector['.red'];
+const green = document.querySelector['.green'];
