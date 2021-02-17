00c5f9c002b6d59021079f851c708062ac6b8cd3
Aline Teixeira <alinet.reis1@gmail.com>
2021-02-17 20:49:32 -0300

    Construindo o codigo JS para gerar as cores aleatoriamente e comparando a funçao click com o que foi gerado

--- script.js
+++ script.js
@@ -12,6 +12,8 @@ const green = document.querySelector['.green'];
 //red 2
 //green 3
 
+//gerando as cores aleatoriamente
+
 let shuffleOrder = () => {
     let colorOrder = Math.floor[Math.random()*4];
     order[order.length]=colorOrder;
@@ -19,5 +21,48 @@ let shuffleOrder = () => {
 
     for (let i in order){
         let elementColor = createColorElement (order[i]);
+        lightColor(elementColor, Number[i]+1);
     }
+}
+
+//acende a proxima cor
+
+let lightColor =(element, number) => {
+    number = number * 500;
+
+    setTimeout(() =>{
+        element.classList.add('selected');
+
+    }, number - 250);
+
+    setTimeout(() =>{
+        element.classList.remove('selected');
+    });
+}
+
+//verificar se a cor gerada e a mesma selecionada
+
+let checkOrder = ()=>{
+    for(let i in clickedOrder){
+        if(clickedOrder[i] != order[i]){
+            lose();
+            break;
+        }
+    }
+        if(clickedOrder.length==order.length){
+            alert(`Pontuação: ${score}\nVoce acertou, iniciando proximo nível`);
+        }
+}
+
+//função clicar
+
+let click =(color) =>{
+    clickedOrder[clickedOrder.length]=color;
+    elementColor(color).classList.add('selected');
+
+    setTimeout(() => {
+        elementColor(color).classList.remove('selected');
+        checkOrder();
+
+     });
 }
